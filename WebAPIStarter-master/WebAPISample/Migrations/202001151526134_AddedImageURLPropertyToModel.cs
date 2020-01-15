444061a6da0da0a1e65077c098d0d4d5c09d2389
Bryce Sickles <Brycesickles@gmail.com>
2020-01-15 10:46:00 -0600

    added functionality to display images alongside movies in list. Uploaded formatted movie images to IMAGES folder and adjusted model to include image path string. From there, the homepage was edited with css styling to make a more aesthetically pleasing experience.

---
+++ WebAPIStarter-master/WebAPISample/Migrations/202001151526134_AddedImageURLPropertyToModel.cs
@@ -0,0 +1,18 @@
+namespace WebAPISample.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddedImageURLPropertyToModel : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Movies", "ImageURL", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Movies", "ImageURL");
+        }
+    }
+}
